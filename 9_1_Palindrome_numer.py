521b31e5079e91336956068f399fb782d8cd07de
henrysaenz <hvalverde@uninorte.edu.co>
2025-12-28 17:20:08 -0500

    Exercise 9 palindrome

---
+++ 9_1_Palindrome_numer.py
@@ -0,0 +1,54 @@
+"""Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-2**31 <= x <= 2**31 - 1
+
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        vector = [int(d) for d in str(x)] #list(map(int, str(x)))
+        n = len(vector)
+        if n%2 == 0:
+            m = n//2
+            for i in range(m):
+                if vector[i] - vector[n-1-i] ==0:
+                    m=m-1
+            if m == 0:
+                return True
+            else:
+                return False
+        else:
+            m = n//2
+            for i in range(m):
+                if vector[i] - vector[n-1-i] ==0:
+                    m=m-1
+            if m == 0:
+                return True
+            else:
+                return False
+            
+sol = Solution()
+print(sol.isPalindrome(27842))
