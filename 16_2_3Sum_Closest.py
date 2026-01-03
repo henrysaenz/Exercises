126d43cf32e201721e14a0d63b90335ee891b533
henrysaenz <hvalverde@uninorte.edu.co>
2026-01-03 18:37:34 -0500

    3Sum closest

---
+++ 16_2_3Sum_Closest.py
@@ -0,0 +1,64 @@
+'''
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+'''
+
+
+from typing import List
+from itertools import combinations
+
+from typing import List
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest_sum = nums[0] + nums[1] + nums[2]
+
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+
+                if abs(current_sum - target) < abs(closest_sum - target):
+                    closest_sum = current_sum
+
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return target 
+
+        return closest_sum
+
+            
+if __name__ == "__main__":
+    sol = Solution()
+    resultado = sol.threeSumClosest([-1, 2, 1, -4], 1)
+    print(resultado)  
